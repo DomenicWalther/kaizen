3ee41ed5d6a4760bbd5d52c86597199f48d69b66
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2026-01-01 22:29:30 +0100

    feat: add basic routing & nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    loadComponent: () => {
+      return import('./pages/dashboard/dashboard').then((m) => m.Dashboard);
+    },
+  },
+  {
+    path: 'character',
+    loadComponent: () => {
+      return import('./pages/character/character').then((m) => m.Character);
+    },
+  },
+  {
+    path: 'milestones',
+    loadComponent: () => {
+      return import('./pages/milestones/milestones').then((m) => m.Milestones);
+    },
+  },
+  {
+    path: 'campaign',
+    loadComponent: () => {
+      return import('./pages/campaign/campaign').then((m) => m.Campaign);
+    },
+  },
+];
---
+++ src/app/pages/milestones/milestones.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-milestones',
+  imports: [],
+  templateUrl: './milestones.html',
+  styleUrl: './milestones.css',
+})
+export class Milestones {
+
+}

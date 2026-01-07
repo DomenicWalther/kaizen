a93e57a7a9446db0db893b9edd39a67ac8943839
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2026-01-07 17:03:11 +0100

    feat(campaign): add sprite and fighting service

--- src/app/pages/campaign/campaign.ts
+++ src/app/pages/campaign/campaign.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { CharacterService } from '../../services/character';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-campaign',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './campaign.html',
   styleUrl: './campaign.css',
 })
 export class Campaign {
+  characterService = inject(CharacterService);
+  get character() {
+    return this.characterService.character;
+  }
 
+  attackSpeed = 1000;
+
+  enemyHP = signal(100);
+
+  performAttack() {
+    let attackAmount =
+      this.character().baseStrength *
+      this.character().strengthModifier *
+      this.character().prestigeMultipliers.strength;
+    this.enemyHP.update((hp) => Math.max(0, hp - attackAmount));
+    if (this.enemyHP() === 0) {
+      this.enemyHP.set(100);
+      this.characterService.modifyStat('gold', 50);
+      this.character().currentWave += this.character().currentWave < 10 ? 1 : -9;
+    }
+  }
+
+  beginFight() {
+    setInterval(() => {
+      this.performAttack();
+    }, this.attackSpeed);
+  }
 }

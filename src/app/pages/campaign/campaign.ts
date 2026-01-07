4e8d0f05bde64f55af15e1d4bacee8493b7fb993
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2026-01-08 00:04:25 +0100

    feat(campaign): add ability to end fighting, fixed calculation for DPS & Attack Speed, added scaling to enemy HP based on Stage & Wave. Refactored the way enemyHP and killing of enemies is handled

--- src/app/pages/campaign/campaign.ts
+++ src/app/pages/campaign/campaign.ts
@@ -1,41 +1,81 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { CharacterService } from '../../services/character';
 import { NgOptimizedImage } from '@angular/common';
+import { Character } from '../../models/character.model';
 
 @Component({
   selector: 'app-campaign',
   imports: [NgOptimizedImage],
   templateUrl: './campaign.html',
-  styleUrl: './campaign.css',
 })
 export class Campaign {
   characterService = inject(CharacterService);
+  isFighting = signal(false);
+  fightIntervalID: any;
   get character() {
     return this.characterService.character;
   }
 
   attackSpeed = signal(1000);
-  attackDamage = signal(
-    this.character().baseStrength *
-      this.character().strengthModifier *
-      this.character().prestigeMultipliers.strength
-  );
-  damagePerSecond = signal(this.attackDamage() / (this.attackSpeed() / 1000));
-  enemyHP = signal(100);
+  attackDamage = computed(() => {
+    const char = this.character();
+    const baseDamage =
+      char.baseStrength * char.strengthModifier * char.prestigeMultipliers.strength;
+    return baseDamage;
+  });
+  damagePerSecond = computed(() => this.attackDamage() * (1000 / this.attackSpeed()));
+  enemyHP = signal(this.calculateEnemyHP());
 
   performAttack() {
     let attackAmount = this.attackDamage();
     this.enemyHP.update((hp) => Math.max(0, hp - attackAmount));
     if (this.enemyHP() === 0) {
-      this.enemyHP.set(100);
-      this.characterService.modifyStat('gold', 50);
-      this.character().currentWave += this.character().currentWave < 10 ? 1 : -9;
+      this.handleEnemyDefeat();
     }
   }
 
-  beginFight() {
-    setInterval(() => {
-      this.performAttack();
-    }, this.attackSpeed());
+  handleEnemyDefeat() {
+    this.enemyHP.set(this.calculateEnemyHP());
+    this.characterService.modifyStat('gold', 50);
+    if (this.character().currentWave == 10) {
+      this.character().currentStage = 100;
+    }
+    this.character().currentWave += this.character().currentWave < 10 ? 1 : -9;
+  }
+
+  calculateEnemyHP() {
+    const baseHP = 100;
+    const stageMultiplier = 1.6;
+    const waveMultiplier = 0.05;
+
+    const stagePower = Math.pow(stageMultiplier, this.character().currentStage - 1);
+    const wavePower = Math.pow(1 + waveMultiplier, this.character().currentWave - 1);
+    return Math.floor(baseHP * stagePower * wavePower);
+  }
+
+  increaseAttackSpeed() {
+    this.attackSpeed.update((speed) => speed / 2);
+  }
+
+  toggleFight() {
+    if (!this.isFighting()) {
+      this.fightIntervalID = setInterval(() => {
+        this.performAttack();
+      }, this.attackSpeed());
+      this.isFighting.set(true);
+    } else {
+      clearInterval(this.fightIntervalID);
+      this.isFighting.set(false);
+    }
+  }
+
+  modifyStat(
+    stat: keyof Pick<
+      Character,
+      'level' | 'baseStrength' | 'baseIntelligence' | 'baseEndurance' | 'gold'
+    >,
+    amount: number
+  ) {
+    this.characterService.modifyStat(stat, amount);
   }
 }

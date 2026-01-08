442bd9ca7de7d901f3240aa47f2c34430c097b52
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2026-01-08 23:38:10 +0100

    feat(prestige): add basic prestige interface

---
+++ src/app/models/prestige.model.ts
@@ -0,0 +1,28 @@
+export interface PrestigeUpgrade {
+  id: string;
+  name: string;
+  description: string;
+
+  baseCost: number;
+  costScaling: number;
+
+  effectType: UpgradeEffectType;
+  effectValue: number;
+  effectScaling: UpgradeScalingType;
+
+  currentLevel: number;
+}
+
+enum UpgradeEffectType {
+  FLAT_STAT_BOOST = 'flat_stat_boost',
+  MULTIPLIER_BOOST = 'multiplier_boost',
+  ATTACK_SPEED = 'attack_speed',
+  ENEMY_HEALTH_REDUCTION = 'enemy_health_reduction',
+  DYNAMIC_PER_CORE = 'dynamic_per_core',
+}
+
+enum UpgradeScalingType {
+  LINEAR = 'linear',
+  EXPONENTIAL = 'exponential',
+  FIXED_PER_LEVEL = 'fixed_per_level',
+}

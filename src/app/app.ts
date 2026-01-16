8baab11a7ba61be7983726e66722219b56446d42
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2026-01-16 01:11:55 +0100

    feat(AutoSave): implement AutoSave Interval - doesn't save anything to DB yet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,23 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { StoreUserService } from './services/store-user-service';
+import { AutoSaveService } from './services/autosave-service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
 })
-export class App {
-  protected readonly title = signal('kaizen');
+export class App implements OnInit, OnDestroy {
+  constructor(private autoSaveService: AutoSaveService) {}
+
+  ngOnInit() {
+    this.autoSaveService.startAutoSave();
+  }
+
+  ngOnDestroy() {
+    this.autoSaveService.stopAutoSave();
+  }
   _ = inject(StoreUserService);
 }

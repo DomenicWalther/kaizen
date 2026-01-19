ad7b7bc6658d52b3a9de03aa944488d2c5eb4be5
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2026-01-19 15:46:31 +0100

    styling(navbar): add basic navbar animation

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
+  styleUrl: './navbar-style.css',
 })
-export class Navbar {}
+export class Navbar implements AfterViewInit {
+  @ViewChild('nav') nav!: ElementRef;
+  @ViewChild('underline') underline!: ElementRef;
+
+  constructor(private router: Router) {}
+  ngAfterViewInit() {
+    this.moveUnderline();
+
+    this.router.events.pipe(filter((e) => e instanceof NavigationEnd)).subscribe(() => {
+      setTimeout(() => this.moveUnderline());
+    });
+  }
+
+  moveUnderline() {
+    console.log('Moving underline!');
+    const active = this.nav.nativeElement.querySelector('.active-link');
+    console.log(active);
+    if (!active) return;
+
+    const rect = active.getBoundingClientRect();
+    const parentRect = this.nav.nativeElement.getBoundingClientRect();
+
+    const left = rect.left - parentRect.left;
+    const width = rect.width;
+
+    const el = this.underline.nativeElement;
+    el.style.left = `${left}px`;
+    el.style.width = `${width}px`;
+  }
+}
